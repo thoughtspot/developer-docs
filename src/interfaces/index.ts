d64d47acecc9311b12b857f35e26ae157c0e3766
tonyjames-thoughtspot <123717820+tonyjames-thoughtspot@users.noreply.github.com>
2023-01-29 21:34:32 +0530

    Initial commit

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export interface SearchQueryResult {
+    pageid: string;
+    title: string;
+    type: string;
+    link: string;
+    sectionId: string;
+    _snippetResult: any;
+    sectionTitle: string,
+}
