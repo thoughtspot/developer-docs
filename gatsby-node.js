c406d9fe1871dcf627833f59e8cbaefb7ee4e048
Justin Mathew <justin.mathew@thoughtspot.com>
2023-04-13 13:02:37 +0530

    Added template to generate page statically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,38 @@
 const fsExtra = require('fs-extra');
+const { DOC_NAV_PAGE_ID } = require('./src/configs/doc-configs');
 
 exports.onPostBuild = () => {
-
-    fsExtra.copyFileSync(`${__dirname}/robots.txt`, `${__dirname}/public/robots.txt`);
+    fsExtra.copyFileSync(
+        `${__dirname}/robots.txt`,
+        `${__dirname}/public/robots.txt`,
+    );
+};
+exports.createPages = async function ({ actions, graphql }) {
+    const { data } = await graphql(`
+        query {
+            allAsciidoc {
+                edges {
+                    node {
+                        html
+                        document {
+                            title
+                        }
+                        pageAttributes {
+                            pageid
+                        }
+                    }
+                }
+            }
+        }
+    `);
+    data.allAsciidoc.edges.forEach((edge) => {
+        const { pageid: pageId } = edge.node.pageAttributes;
+        actions.createPage({
+            path: `/${pageId}`,
+            component: require.resolve(
+                './src/components/DevDocTemplate/index.tsx',
+            ),
+            context: { pageId, navId: DOC_NAV_PAGE_ID },
+        });
+    });
 };
