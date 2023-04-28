e6401a7554018b375d89909e355ee1ba50fdbbe2
sastaachar <justinjohnmathew100@gmail.com>
2023-04-28 08:37:05 +0530

    Fixed left nav and updated algolia

--- src/components/LeftSidebar/index.tsx
+++ src/components/LeftSidebar/index.tsx
@@ -6,7 +6,7 @@ import { GiHamburgerMenu } from '@react-icons/all-files/gi/GiHamburgerMenu';
 import { MdClear } from '@react-icons/all-files/md/MdClear';
 import { RiMoonClearLine } from '@react-icons/all-files/ri/RiMoonClearLine';
 import { FiSun } from '@react-icons/all-files/fi/FiSun';
-import queryStringParser from '../../utils/app-utils';
+import { removeTrailingSlash, queryStringParser } from '../../utils/app-utils';
 import { NAV_PREFIX, TS_PAGE_ID_PARAM } from '../../configs/doc-configs';
 import {
     LEFT_NAV_WIDTH_DESKTOP,
@@ -18,7 +18,11 @@ import {
     MIN_LEFT_NAV_WIDTH_TABLET,
     MAX_MOBILE_RESOLUTION,
 } from '../../constants/uiConstants';
-import { collapseAndExpandLeftNav, addExpandCollapseImages } from './helper';
+import {
+    collapseAndExpandLeftNav,
+    addExpandCollapseImages,
+    getPageIdFromUrl,
+} from './helper';
 import NavContent from './NavContent';
 import './index.scss';
 
@@ -48,9 +52,10 @@ const LeftSideBar = (props: {
     useEffect(() => {
         const divElement = document.createElement('div');
         divElement.innerHTML = props.navContent;
-        const tag = divElement.querySelector(
-            `a[href='${props.location.pathname}']`,
-        );
+        const pageid =
+            removeTrailingSlash(props.location.pathname).replace(/^\/*/, '') ||
+            getPageIdFromUrl(props.location.href);
+        const tag = divElement.querySelector(`a[href='/${pageid}']`);
         if (tag) {
             tag.classList.add('active');
         }
@@ -109,7 +114,7 @@ const LeftSideBar = (props: {
                 ]}
                 axis="x"
                 className="resizable"
-                height={height || 0} //Height is set to 0 when element is not rendered
+                height={height || 0} // Height is set to 0 when element is not rendered
             >
                 <NavContent
                     backLink={props.backLink}
@@ -124,49 +129,55 @@ const LeftSideBar = (props: {
                 />
             </ResizableBox>
         ) : (
-                <div className="menuMain">
-                    <div className="menuContainer">
-                        <IconContext.Provider
-                            value={{
-                                className: `icon ${props.leftNavOpen && 'imgOpacity'
-                                    }`,
-                            }}
-                        >
-                            <GiHamburgerMenu onClick={onMenuClick} />
-                        </IconContext.Provider>
-                        <IconContext.Provider
-                            value={{
-                                className: `icon ${props.leftNavOpen && 'imgOpacity'
-                                    }`,
-                            }}
-                        >
-                            {props.isDarkMode
-                                ? <RiMoonClearLine onClick={() => props.setDarkMode(false)} />
-                                : <FiSun onClick={() => props.setDarkMode(true)} />
-                            }
-                        </IconContext.Provider>
-                        <IconContext.Provider
-                            value={{
-                                className: `icon ${!props.leftNavOpen && 'imgOpacity'
-                                    } clearIcon`,
-                            }}
-                        >
-                            <MdClear onClick={onMenuClick} />
-                        </IconContext.Provider>
-                    </div>
-                    <NavContent
-                        backLink={props.backLink}
-                        navContent={navContent}
-                        navTitle={props.navTitle}
-                        refObj={ref as React.RefObject<HTMLDivElement>}
-                        leftNavOpen={props.leftNavOpen}
-                        isPublicSiteOpen={props.isPublicSiteOpen}
-                        isMaxMobileResolution={isMaxMobileResolution}
-                        setDarkMode={props.setDarkMode}
-                        isDarkMode={props.isDarkMode}
-                    />
+            <div className="menuMain">
+                <div className="menuContainer">
+                    <IconContext.Provider
+                        value={{
+                            className: `icon ${
+                                props.leftNavOpen && 'imgOpacity'
+                            }`,
+                        }}
+                    >
+                        <GiHamburgerMenu onClick={onMenuClick} />
+                    </IconContext.Provider>
+                    <IconContext.Provider
+                        value={{
+                            className: `icon ${
+                                props.leftNavOpen && 'imgOpacity'
+                            }`,
+                        }}
+                    >
+                        {props.isDarkMode ? (
+                            <RiMoonClearLine
+                                onClick={() => props.setDarkMode(false)}
+                            />
+                        ) : (
+                            <FiSun onClick={() => props.setDarkMode(true)} />
+                        )}
+                    </IconContext.Provider>
+                    <IconContext.Provider
+                        value={{
+                            className: `icon ${
+                                !props.leftNavOpen && 'imgOpacity'
+                            } clearIcon`,
+                        }}
+                    >
+                        <MdClear onClick={onMenuClick} />
+                    </IconContext.Provider>
                 </div>
-            );
+                <NavContent
+                    backLink={props.backLink}
+                    navContent={navContent}
+                    navTitle={props.navTitle}
+                    refObj={ref as React.RefObject<HTMLDivElement>}
+                    leftNavOpen={props.leftNavOpen}
+                    isPublicSiteOpen={props.isPublicSiteOpen}
+                    isMaxMobileResolution={isMaxMobileResolution}
+                    setDarkMode={props.setDarkMode}
+                    isDarkMode={props.isDarkMode}
+                />
+            </div>
+        );
     };
 
     return renderLeftNav();
--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -1,37 +1,37 @@
-const { JSDOM }  = require("jsdom");
-const { htmlToText } = require("html-to-text");
+const { JSDOM } = require('jsdom');
+const { htmlToText } = require('html-to-text');
 const config = require('../configs/doc-configs');
 const { getAlgoliaIndex } = require('../configs/algolia-search-config');
 
 const getPathPrefix = () => {
-  return 'docs';
+    return 'docs';
 };
 
 const getPath = (path) =>
     getPathPrefix() ? `${path}/${getPathPrefix()}` : path;
 
 const stripLinks = (text) => {
-  if (text) {
-      const re = /<a\s.*?href=[\"\'](.*?)[\"\']*?>(.*?)<\/a>/g;
-      const str = text;
-      const subst = '$2';
-      const result = str.replace(re, subst);
-      return result;
-  }
-  return '';
+    if (text) {
+        const re = /<a\s.*?href=[\"\'](.*?)[\"\']*?>(.*?)<\/a>/g;
+        const str = text;
+        const subst = '$2';
+        const result = str.replace(re, subst);
+        return result;
+    }
+    return '';
 };
 
 const getTextFromHtml = (html) => {
     const text = htmlToText(`${html}`, {
         selectors: [
-            { selector: "a", options: { ignoreHref: true } },
-            { selector: "h1", format: "skip" },
-            { selector: "h2", format: "skip" },
-            { selector: "h3", format: "skip" },
-        ]
+            { selector: 'a', options: { ignoreHref: true } },
+            { selector: 'h1', format: 'skip' },
+            { selector: 'h2', format: 'skip' },
+            { selector: 'h3', format: 'skip' },
+        ],
     });
     return text;
-}
+};
 
 const pageQuery = `
 query {
@@ -69,32 +69,49 @@ query {
 }
 `;
 
-const pageToAlgoliaRecordForASCII = (ele, type , node) => {
+const pageToAlgoliaRecordForASCII = (ele, type, node) => {
     const pageid = node.pageAttributes.pageid;
-    let sectionId,sectionTitle;
-    if(type === 'section') {
+    let sectionId;
+    let sectionTitle;
+    if (type === 'section') {
         sectionId = ele.querySelector('h2').id;
         sectionTitle = ele.querySelector('h2').innerHTML;
     } else {
         sectionId = type;
         sectionTitle = node.document.title;
     }
+
+    let body = ele && getTextFromHtml(ele.innerHTML);
+    const len = new TextEncoder().encode(body).length;
+
+    if (len > 8000) {
+        body = '';
+    }
+
     return {
         objectID: node.id + sectionId,
         sectionId,
         sectionTitle,
-        body: ele && getTextFromHtml(ele.innerHTML),
+        body,
         pageid,
         type: 'ASCII',
         title: node.document.title,
-    }
-}
+        link: `/${pageid}`,
+    };
+};
 
 const pageToAlgoliaRecordForTypedoc = (node) => {
     const pageid = node.name;
-    const body = node && node.childHtmlRehype? getTextFromHtml(
-        node.childHtmlRehype.html,
-    ): '';
+    let body =
+        node && node.childHtmlRehype
+            ? getTextFromHtml(node.childHtmlRehype.html)
+            : '';
+
+    const len = new TextEncoder().encode(body).length;
+    // ignoring big objects for now
+    if (len > 8000) {
+        body = '';
+    }
     return {
         objectID: node.id,
         body,
@@ -102,51 +119,63 @@ const pageToAlgoliaRecordForTypedoc = (node) => {
         typedoc: true,
         type: node.extension,
         title: node.childHtmlRehype.htmlAst.children.find(
-            (children) =>
-                children.tagName === 'title',
+            (children) => children.tagName === 'title',
         ).children[0].value,
-        link: `${getPath(config.DOC_REPO_NAME)}/${
-            config.TYPE_DOC_PREFIX
-        }/${node.relativePath}`,
+        link: `${getPath(config.DOC_REPO_NAME)}/${config.TYPE_DOC_PREFIX}/${
+            node.relativePath
+        }`,
     };
-}
+};
 
 const queries = [
-  {
-    query: pageQuery,
-    transformer: ({ data }) => {
-      return [
-        ...data.allAsciidoc.edges
-            .filter(
-                (edge) =>
-                    edge.node.pageAttributes.pageid &&
-                    edge.node.pageAttributes.pageid !== 'nav',
-            )
-            .reduce((acc,edge) => {
-                const newDiv = new JSDOM(`<div>${edge.node.html}</div>`).window.document;
-                const preambleEle = newDiv.querySelector('#preamble');
-                const preamble = preambleEle ? pageToAlgoliaRecordForASCII(preambleEle, 'preamble',edge.node) : null;
-                const sections = Array.prototype.map.call(newDiv.querySelectorAll('.sect1'),(sect) => 
-                    pageToAlgoliaRecordForASCII(sect,'section',edge.node)
-                );
-                if(preamble) {
-                    acc = [...acc,preamble];
-                }
-                return [...acc,...sections];
-            },[]),
-        ...data.allFile.edges
-            .filter((edge) => edge.node.extension === 'html')
-            .map((edge) => {
-                return edge && pageToAlgoliaRecordForTypedoc(edge.node);
-            }),
-      ];
+    {
+        query: pageQuery,
+        transformer: ({ data }) => {
+            return [
+                ...data.allAsciidoc.edges
+                    .filter(
+                        (edge) =>
+                            edge.node.pageAttributes.pageid &&
+                            edge.node.pageAttributes.pageid !== 'nav',
+                    )
+                    .reduce((acc, edge) => {
+                        const newDiv = new JSDOM(`<div>${edge.node.html}</div>`)
+                            .window.document;
+                        const preambleEle = newDiv.querySelector('#preamble');
+                        const preamble = preambleEle
+                            ? pageToAlgoliaRecordForASCII(
+                                  preambleEle,
+                                  'preamble',
+                                  edge.node,
+                              )
+                            : null;
+                        const sections = Array.prototype.map.call(
+                            newDiv.querySelectorAll('.sect1'),
+                            (sect) =>
+                                pageToAlgoliaRecordForASCII(
+                                    sect,
+                                    'section',
+                                    edge.node,
+                                ),
+                        );
+                        if (preamble) {
+                            acc = [...acc, preamble];
+                        }
+                        return [...acc, ...sections];
+                    }, []),
+                // ...data.allFile.edges
+                //     .filter((edge) => edge.node.extension === 'html')
+                //     .map((edge) => {
+                //         return edge && pageToAlgoliaRecordForTypedoc(edge.node);
+                //     }),
+            ];
+        },
+        indexName: getAlgoliaIndex(),
+        settings: {
+            attributesToSnippet: ['body:10'],
+        },
     },
-    indexName: getAlgoliaIndex(),
-    settings: {
-        attributesToSnippet: ['body:10'],
-    },
-  },
-]
+];
 
 module.exports = {
     queries,
