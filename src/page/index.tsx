75f4661a03ecc2b62ab1f139dc244716add71223
Rijad R <rijad.r@thoughtspot.com>
2023-05-30 13:13:34 +0530

    get parent host url fix

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -272,7 +272,18 @@ const IndexPage = ({ location }) => {
     const isExternal = () =>
         !location?.href?.includes('developers.thoughtspot.com/docs');
 
-    const baseUrl = isExternal() ? location?.origin : DEFAULT_HOST;
+    let parentUrl = location?.origin;
+
+    if (typeof window !== 'undefined') {
+        const { ancestorOrigins } = window?.location;
+
+        parentUrl =
+            ancestorOrigins?.length > 0
+                ? ancestorOrigins[ancestorOrigins?.length - 1]
+                : document.referrer || window?.origin;
+    }
+    const baseUrl = isExternal() ? parentUrl : DEFAULT_HOST;
+
     const playgroundUrl =
         clusterType === CLUSTER_TYPES.PROD
             ? playgroundUrlTemplate({ version: DOC_VERSION_PROD })
@@ -367,6 +378,7 @@ const IndexPage = ({ location }) => {
                 height="100%"
                 width="100%"
                 onLoad={() => setIsPlaygroundReady(true)}
+                id="iframe"
             />
         </div>
     );
