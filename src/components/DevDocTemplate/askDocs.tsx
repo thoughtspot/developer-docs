19a31b04afb727319138f502daa04153fa1f3628
Nirmay Patel <114509664+Nirmay15@users.noreply.github.com>
2023-08-04 00:35:28 +0530

    Update askDocs.tsx

--- src/components/DevDocTemplate/askDocs.tsx
+++ src/components/DevDocTemplate/askDocs.tsx
@@ -4,7 +4,7 @@ export const AskDocs = () => {
     return (
         <iframe
             style={{ flex: '1 1 0' }}
-            src="https://tse-app-builder-git-copyexamples-thoughtspot-site.vercel.app/complete?embed=true"
+            src="https://tse-app-builder.vercel.app/complete?embed=true"
             frameBorder="0"
             id="iframe"
         />
