b058bf2e0d1cc066750138e4ab01cad8eb1fd776
sastaachar <justinjohnmathew100@gmail.com>
2023-08-30 15:10:02 +0530

    support for empty links in dropdowns

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -4,7 +4,7 @@ import { VERSION_DROPDOWN, TS_Version } from '../../configs/doc-configs';
 
 import './index.scss';
 
-const Dropdown = (props: { location: Location; isMobile: Boolean }) => {
+const Dropdown = (props: { location: Location; isMobile: boolean }) => {
     const { location } = props;
     const options = VERSION_DROPDOWN;
     const [currentVersion, setCurrentVersion] = useState({});
@@ -14,23 +14,19 @@ const Dropdown = (props: { location: Location; isMobile: Boolean }) => {
         const version =
             params.get('version') || localStorage.getItem('version');
         const selectedOption =
-            options.find(({ label }) => {   
-                return label===version;
+            options.find(({ label }) => {
+                return label === version;
             }) || options[0];
         if (selectedOption) setCurrentVersion(selectedOption);
     }, []);
 
-    const handelClick = ({label, link}) => {
+    const handelClick = ({ label, link }) => {
         // const params = new URLSearchParams(location.search);
         // params.set('version', label);
-       
+
         localStorage.setItem('version', label);
         window?.location?.replace(link);
     };
-
-    if (!currentVersion?.link) {
-        return <div />;
-    }
 
     return (
         <div className="dropdownWrapper">
