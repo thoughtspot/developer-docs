a18b1177756f7856456bab4471c982ee78917e00
Bryant Howell - ThoughtSpot <83678239+bryanthowell-ts@users.noreply.github.com>
2024-10-22 14:33:51 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -43,7 +43,7 @@ exports.createPages = async function ({ actions, graphql }) {
     data.allAsciidoc.edges.forEach((edge) => {
         const { pageid: pageId } = edge.node.pageAttributes;
 
-        if (pageId.indexOf('tutorial') == -1) {
+        if (pageId.indexOf("tutorial") == -1) {
             actions.createPage({
                 path: `tutorials/${pageId}`,
                 component: require.resolve(
@@ -52,15 +52,15 @@ exports.createPages = async function ({ actions, graphql }) {
                 context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
             });
         }
-        else {
-            actions.createPage({
-                path: `/${pageId}`,
-                component: require.resolve(
-                    './src/components/DevDocTemplate/index.tsx',
-                ),
-                context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
-            });
-        }
+
+        actions.createPage({
+            path: `/${pageId}`,
+            component: require.resolve(
+                './src/components/DevDocTemplate/index.tsx',
+            ),
+            context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
+        });
+
         if (pageId === 'introduction') {
             actions.createPage({
                 path: '/',
