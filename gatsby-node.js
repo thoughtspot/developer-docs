1a0a3301d9e0edd27bdaef1dd9bd0ed6c66fbfb7
Bryant Howell - ThoughtSpot <83678239+bryanthowell-ts@users.noreply.github.com>
2024-10-25 06:16:17 -0500

    Update gatsby-node.js
    gatsby-node.js additions to bring in the tutorials module and create the pages with a sub-directory structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,8 +26,11 @@ exports.createPages = async function ({ actions, graphql }) {
                         parent {
                             ... on File {
                                 name
+                                sourceInstanceName
+                                relativePath
                             }
                         }
+                        
                     }
                 }
             }
@@ -36,20 +39,65 @@ exports.createPages = async function ({ actions, graphql }) {
 
     const namePageIdMap = {};
     data.allAsciidoc.edges.forEach((e) => {
-        namePageIdMap[e.node.parent.name] =
-            e.node.pageAttributes.pageid || NOT_FOUND_PAGE_ID;
+        const { sourceInstanceName: sourceName, relativePath : relPath } = e.node.parent;
+        const pageId = e.node.pageAttributes.pageid;
+        if (sourceName === 'tutorials'){
+           const relPathSplit = relPath.split('/');
+           const pageIdSplit = pageId.split('_');
+           let finalPageId = pageId;
+           if( pageIdSplit.length > 1) {
+                finalPageId = pageIdSplit[1];
+           }
+
+           if(relPathSplit.length > 1) {
+                const mapPageId = `tutorials/${relPathSplit[0]}/` + finalPageId;
+                namePageIdMap[e.node.parent.name] =
+                   mapPageId || NOT_FOUND_PAGE_ID;
+           }
+        }
+
+        else {
+            namePageIdMap[e.node.parent.name] =
+                e.node.pageAttributes.pageid || NOT_FOUND_PAGE_ID;
+        }
     });
 
     data.allAsciidoc.edges.forEach((edge) => {
         const { pageid: pageId } = edge.node.pageAttributes;
+        const { sourceInstanceName: sourceName, relativePath : relPath } = edge.node.parent;
 
-        actions.createPage({
-            path: `/${pageId}`,
-            component: require.resolve(
-                './src/components/DevDocTemplate/index.tsx',
-            ),
-            context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
-        });
+        // Tutorials module pageids follow pattern {subdirectory}_{final_url_stub} to give unique IDs in system but allow directory structure in URL
+        if (sourceName === 'tutorials'){            
+           // One-level of subdirectory part of stub
+           const relPathSplit = relPath.split('/');
+           const pageIdSplit = pageId.split('_');
+           let finalPageId = pageId;
+           if( pageIdSplit.length > 1) {
+                finalPageId = pageIdSplit[1];
+           }
+
+           const finalPath = `/tutorials/${relPathSplit[0]}/${finalPageId}`;
+
+           if(relPathSplit.length > 1) {
+               actions.createPage({
+                        path: finalPath,
+                        component: require.resolve(
+                            './src/components/DevDocTemplate/index.tsx',
+                        ),
+                        context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
+                    });
+           }
+        }
+
+        else {
+            actions.createPage({
+                path: `/${pageId}`,
+                component: require.resolve(
+                    './src/components/DevDocTemplate/index.tsx',
+                ),
+                context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
+            });
+        }
 
         if (pageId === 'introduction') {
             actions.createPage({
