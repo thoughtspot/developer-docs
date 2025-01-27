db12b6a1ef4f6560c3d3bccdcca16a9d31db46a2
Justin Mathew <42416647+sastaachar@users.noreply.github.com>
2025-01-27 20:30:32 +0530

    SCAL-237361 : fixed tutorial links (#261)

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,7 @@ const {
     DOC_NAV_PAGE_ID,
     NOT_FOUND_PAGE_ID,
 } = require('./src/configs/doc-configs');
+const { getDocLinkFromEdge } = require('./src/utils/gatsby-utils.js');
 
 exports.onPostBuild = () => {
     fsExtra.copyFileSync(
@@ -30,7 +31,6 @@ exports.createPages = async function ({ actions, graphql }) {
                                 relativePath
                             }
                         }
-                        
                     }
                 }
             }
@@ -39,24 +39,24 @@ exports.createPages = async function ({ actions, graphql }) {
 
     const namePageIdMap = {};
     data.allAsciidoc.edges.forEach((e) => {
-        const { sourceInstanceName: sourceName, relativePath : relPath } = e.node.parent;
+        const {
+            sourceInstanceName: sourceName,
+            relativePath: relPath,
+        } = e.node.parent;
         const pageId = e.node.pageAttributes.pageid;
-        if (sourceName === 'tutorials'){
-           const relPathSplit = relPath.split('/');
-           const pageIdSplit = pageId.split('__');
-           let finalPageId = pageId;
-           if( pageIdSplit.length > 1) {
+        if (sourceName === 'tutorials') {
+            const relPathSplit = relPath.split('/');
+            const pageIdSplit = pageId.split('__');
+            let finalPageId = pageId;
+            if (pageIdSplit.length > 1) {
                 finalPageId = pageIdSplit[1];
-           }
-           let mapPageId = `tutorials/` + finalPageId;
-           if(relPathSplit.length > 1) {
-                mapPageId = `tutorials/${relPathSplit[0]}/` + finalPageId;
-           }
-           namePageIdMap[e.node.parent.name] =
-                   mapPageId || NOT_FOUND_PAGE_ID;
-        }
-
-        else {
+            }
+            let mapPageId = `tutorials/${finalPageId}`;
+            if (relPathSplit.length > 1) {
+                mapPageId = `tutorials/${relPathSplit[0]}/${finalPageId}`;
+            }
+            namePageIdMap[e.node.parent.name] = mapPageId || NOT_FOUND_PAGE_ID;
+        } else {
             namePageIdMap[e.node.parent.name] =
                 e.node.pageAttributes.pageid || NOT_FOUND_PAGE_ID;
         }
@@ -64,41 +64,15 @@ exports.createPages = async function ({ actions, graphql }) {
 
     data.allAsciidoc.edges.forEach((edge) => {
         const { pageid: pageId } = edge.node.pageAttributes;
-        const { sourceInstanceName: sourceName, relativePath : relPath } = edge.node.parent;
 
-        // Tutorials module pageids follow pattern {subdirectory}_{final_url_stub} to give unique IDs in system but allow directory structure in URL
-        if (sourceName === 'tutorials'){            
-           // One-level of subdirectory part of stub
-           const relPathSplit = relPath.split('/');
-           const pageIdSplit = pageId.split('__');
-           let finalPageId = pageId;
-           if( pageIdSplit.length > 1) {
-                finalPageId = pageIdSplit[1];
-           }
-
-           let finalPath = `/tutorials/${finalPageId}`;
-           if(relPathSplit.length > 1) {
-               finalPath = `/tutorials/${relPathSplit[0]}/${finalPageId}`;
-           }
-           
-           actions.createPage({
-                    path: finalPath,
-                    component: require.resolve(
-                        './src/components/DevDocTemplate/index.tsx',
-                    ),
-                    context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
-                });
-        }
-
-        else {
-            actions.createPage({
-                path: `/${pageId}`,
-                component: require.resolve(
-                    './src/components/DevDocTemplate/index.tsx',
-                ),
-                context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
-            });
-        }
+        const docPath = getDocLinkFromEdge(edge);
+        actions.createPage({
+            path: docPath,
+            component: require.resolve(
+                './src/components/DevDocTemplate/index.tsx',
+            ),
+            context: { pageId, navId: DOC_NAV_PAGE_ID, namePageIdMap },
+        });
 
         if (pageId === 'introduction') {
             actions.createPage({
