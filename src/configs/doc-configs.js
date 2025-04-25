9caa55e4bbbde1ebf4cced0bd3768d36b3013005
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2025-04-25 22:05:43 +0530

    sw link ref removal

--- src/configs/doc-configs.js
+++ src/configs/doc-configs.js
@@ -45,12 +45,6 @@ module.exports = {
             subLabel: 'Cloud',
             iframeUrl: 'https://developer-docs-10-6-0-cl.vercel.app/docs',
         },
-        {
-            label: '10.1.0.sw',
-            link: '10-1-0-sw',
-            subLabel: 'Software (Latest)',
-            iframeUrl: 'https://visual-embed-sdk-10-1.vercel.app/docs',
-        },
     ],
     CUSTOM_PAGE_ID: {
         API_PLAYGROUND: 'restV2-playground',
