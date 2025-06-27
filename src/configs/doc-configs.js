d7e98989f3301544db3a9ada1af17e8287763a3c
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2025-06-27 17:57:44 +0530

    url link fix

--- src/configs/doc-configs.js
+++ src/configs/doc-configs.js
@@ -41,7 +41,7 @@ module.exports = {
         },
         {
              label: '10.9.0.cl',
-             link: ' ',
+             link: '10.9.0.cl',
              subLabel: 'Cloud',
              iframeUrl: 'https://developer-docs-10-9-0-cl.vercel.app/docs/',
          },
