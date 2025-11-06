ea4c3857d604573a675063691038b7afaf725d1b
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2025-11-06 14:39:50 +0530

    version drop-down check

--- src/configs/doc-configs.js
+++ src/configs/doc-configs.js
@@ -41,7 +41,7 @@ module.exports = {
         },
         {
             label: '10.13.0.cl',
-            link: ' ',
+            link: '10.13.0.cl',
             subLabel: 'Cloud',
             iframeUrl: 'https://developer-docs-10-13-0-cl.vercel.app/docs/',
         },
