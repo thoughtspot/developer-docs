9bb49b69da2d4e74da3cc18348319632b7d8ce45
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2026-03-31 21:38:48 +0530

    26.4.0.cl doc updates

--- src/configs/doc-configs.js
+++ src/configs/doc-configs.js
@@ -22,8 +22,8 @@ module.exports = {
         //   'https://developers.thoughtspot.com/docs/26.3.0.cl?pageid=whats-new'
         // - GA: ' /docs/whats-new'
         //linkHref: '/docs/whats-new',
-        linkHref: '/docs/26.3.0.cl?pageid=whats-new',
-        linkText: 'Version 26.3.0.cl',
+        linkHref: '/docs/26.4.0.cl?pageid=whats-new',
+        linkText: 'Version 26.4.0.cl',
         openInNewTab: true,
     },
     TYPE_DOC_PREFIX: 'typedoc',
@@ -48,46 +48,10 @@ module.exports = {
     },
     VERSION_DROPDOWN: [
         {
-            label: '26.3.0.cl',
+            label: '26.4.0.cl',
             link: ' ',
             subLabel: 'Cloud (Latest)',
-            iframeUrl: 'https://developer-docs-26-3-0-cl.vercel.app/docs/',
-        },
-        {
-            label: '26.3.0.cl',
-            link: '26.3.0.cl',
-            subLabel: 'Cloud (Coming soon)',
-            iframeUrl: 'https://developer-docs-26-3-0-cl.vercel.app/docs/',
-        },
-        {
-            label: '10.15.0.cl',
-            link: '10.15.0.cl',
-            subLabel: 'Cloud',
-            iframeUrl: 'https://developer-docs-10-15-0-cl.vercel.app/docs/',
-        },
-        {
-            label: '10.14.0.cl',
-            link: '10.14.0.cl',
-            subLabel: 'Cloud',
-            iframeUrl: 'https://developer-docs-10-14-0-cl.vercel.app/docs/',
-        },
-        {
-            label: '10.13.0.cl',
-            link: '10.13.0.cl',
-            subLabel: 'Cloud',
-            iframeUrl: 'https://developer-docs-10-13-0-cl.vercel.app/docs/',
-        },
-        {
-            label: '10.10.0.sw',
-            link: '10.10.0.sw',
-            subLabel: 'Software (Latest)',
-            iframeUrl: 'https://visual-embed-sdk-10-10.vercel.app/docs/',
-        },
-        {
-            label: '10.1.0.sw',
-            link: '10.1.0.sw',
-            subLabel: 'Software',
-            iframeUrl: 'https://visual-embed-sdk-10-1.vercel.app/docs/',
+            iframeUrl: 'https://developer-docs-26-4-0-cl.vercel.app/docs/',
         },
     ],
     CUSTOM_PAGE_ID: {
