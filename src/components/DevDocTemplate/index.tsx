d1a77355406017263511fca94279d5820897210f
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2026-05-11 20:13:53 +0530

    version iframe header

--- src/components/DevDocTemplate/index.tsx
+++ src/components/DevDocTemplate/index.tsx
@@ -157,11 +157,6 @@ const DevDocTemplate: FC<DevDocTemplateProps> = (props) => {
             version.iframeUrl === props?.pageContext?.iframeUrl,
     );
 
-    // True when loaded inside a VersionIframe (outer shell sets ?_iframe=1).
-    // Outer shell already renders SecondaryHeader, so suppress ours to avoid duplication.
-    const isIframeMode =
-        typeof window !== 'undefined' &&
-        new URLSearchParams(location.search).get('_iframe') === '1';
 
     const isGQPlayGround =
         params[TS_PAGE_ID_PARAM] === CUSTOM_PAGE_ID.GQ_PLAYGROUND;
@@ -723,24 +718,20 @@ const DevDocTemplate: FC<DevDocTemplateProps> = (props) => {
                             : { height: '0px' }
                     }
                 ></div>
-                {!isIframeMode && (
-                    <SecondaryHeader
-                        activeCategory={activeCategory}
-                        onCategoryChange={setActiveCategory}
-                        location={location}
-                        leftNavOpen={leftNavOpen}
-                        setLeftNavOpen={setLeftNavOpen}
-                    />
-                )}
+                <SecondaryHeader
+                    activeCategory={activeCategory}
+                    onCategoryChange={setActiveCategory}
+                    location={location}
+                    leftNavOpen={leftNavOpen}
+                    setLeftNavOpen={setLeftNavOpen}
+                />
                 <main
                     className={getClassName()}
                     ref={ref as React.RefObject<HTMLDivElement>}
                     style={
-                        isIframeMode
-                            ? { height: '100lvh' }
-                            : !isPublicSiteOpen
-                                ? { height: 'calc(100lvh - 44px)' }
-                                : { height: 'calc(100lvh - 65px - 44px)' }
+                        !isPublicSiteOpen
+                            ? { height: 'calc(100lvh - 44px)' }
+                            : { height: 'calc(100lvh - 65px - 44px)' }
                     }
                 >
                     {isPlayGround ? (
