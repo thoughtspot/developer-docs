0b9b8f10243d0faafdf7ae47e339952db6188c5b
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2026-05-11 23:05:33 +0530

    rel version update

--- src/configs/doc-configs.js
+++ src/configs/doc-configs.js
@@ -49,13 +49,13 @@ module.exports = {
     VERSION_DROPDOWN: [
 	    {
             label: '26.5.0.cl',
-            link: '26.5.0.cl',
+            link: ' ',
             subLabel: 'Cloud (Latest)',
             iframeUrl: 'https://developer-docs-26-5-0-cl.vercel.app/docs/',
         },
         {
             label: '26.4.0.cl',
-            link: ' ',
+            link: '26.4.0.cl ',
             subLabel: 'Cloud',
             iframeUrl: 'https://developer-docs-26-4-0-cl.vercel.app/docs/',
         },
