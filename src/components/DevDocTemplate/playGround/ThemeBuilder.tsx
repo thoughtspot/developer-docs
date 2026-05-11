f20dd0b322029a5dc4d5bd3402f1d356c3fd517f
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2026-05-12 01:07:17 +0530

    edits

---
+++ src/components/DevDocTemplate/playGround/ThemeBuilder.tsx
@@ -0,0 +1,3 @@
+// ThemeBuilder not available in this branch
+// import React from 'react';
+// export const ThemeBuilder: React.FC = () => null;
---
+++ src/components/VersionIframe/index.tsx
@@ -0,0 +1,4 @@
+// VersionIframe not available in this branch
+// import React from 'react';
+// const VersionIframe: React.FC<any> = () => null;
+// export default VersionIframe;
