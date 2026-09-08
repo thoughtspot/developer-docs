205114b34600a2327bc2afa843e7693b27f53442
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2026-09-08 22:01:11 +0530

    404 issue fix with /docs path

--- src/components/Page404/index.tsx
+++ src/components/Page404/index.tsx
@@ -4,15 +4,13 @@ import { NOT_FOUND_GO_HOME_PAGE_ID } from '../../configs/doc-configs';
 import t from '../../utils/lang-utils';
 import './index.scss';
 
-// This page is Gatsby's static 404 fallback s
 const Page404 = () => {
     return (
         <main className="pageStyles" id="page-404">
             <title>{t('404_PAGE_TITLE')}</title>
             <h1 className="headingStyles">{t('404_PAGE_HEADING')}</h1>
-            {/* Page 404 message */}
             <p className="paragraphStyles">
-                {`${t('404_PAGE_MSG_PRETEXT')} ${t('404_PAGE_MSG')}`}
+                {t('404_PAGE_MSG_PRETEXT')} {t('404_PAGE_MSG')}
             </p>
             <p className="paragraphStyles">
                 <Link to={`/?pageid=${NOT_FOUND_GO_HOME_PAGE_ID}`}>
